9e93f255c285d0f755e27c7503d524cf08ed807d
PerchunPak <68118654+PerchunPak@users.noreply.github.com>
2021-08-18 21:50:42 +0300

    Убрал токен
    Можете его не пробовать, дискорд сбрасывает если они публикуются на гитхабе в общем доступе

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 debug = True
 
 # Дискорд токен бота
-TOKEN = "ODM0MTc5Mzk5NDIyNzA1NzI0.YH9IGw.iB285ySqDOMlzFVDnc-NICFWzT4"
+TOKEN = ""
 
 # Данные от Postgres (СУБД). Это необходимо, чтобы бот мог хранить данные в базе данных SQL. Данные выглядят так: "postgres://[user]:[password]@[host]:[port]/[database name]" (Некоторые функции не обязательные)
-POSTGRES = 'postgres://test:123123123@localhost:5432/postgres'
+POSTGRES = 'postgres://[user]:[password]@[host]:[port]/[database name]'
