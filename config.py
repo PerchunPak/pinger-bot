c22af22bd1940d6108719651a174b9d33e9e228b
PerchunPak <perchunpak@gmail.com>
2021-08-26 12:40:35 +0300

    Добавил докер

--- config.py
+++
@@ -1,8 +0,0 @@
-# Конфиг бота
-
-# Дискорд токен бота
-TOKEN = ""
-
-# Данные от Postgres (СУБД). Это необходимо, чтобы бот мог хранить данные в базе данных SQL.
-# Данные выглядят так: "postgres://[user]:[password]@[host]:[port]/[database name]" (Некоторые данные не обязательные)
-POSTGRES = 'postgres://[user]:[password]@[host]:[port]/[database name]'
