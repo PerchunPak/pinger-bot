35087c94a1e9c2a83a97fbaea4efd3003ed4a778
PerchunPak <perchunpak@gmail.com>
2021-10-21 22:57:17 +0300

    Заменил коментарии в config.py на docstring-и Перенес вниз чтобы IDE (в моем случае PyCharm) понимал что к чему относится

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-# Конфиг бота
+"""Конфиг бота"""
 
-# Дискорд токен бота
 TOKEN = "TOKEN_HERE"
+"""Дискорд токен бота"""
 
-# Данные от Postgres (СУБД). Это необходимо, чтобы бот мог хранить данные в базе данных SQL.
-# Данные выглядят так: "postgres://[user]:[password]@[host]:[port]/[database name]" (Некоторые данные не обязательные)
 POSTGRES = "postgres://pingerbot:password@localhost:5432/pingerbotdb"
+"""
+Данные от Postgres (СУБД). Это необходимо, чтобы бот мог хранить данные в базе данных SQL.
+Данные выглядят так: "postgres://[user]:[password]@[host]:[port]/[database name]" (Некоторые данные не обязательные)
+"""
