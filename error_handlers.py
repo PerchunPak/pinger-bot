37fcfa0314cd0f2d8df7c23d625288fef0f71fac
PerchunPak <perchunpak@gmail.com>
2021-10-29 22:09:28 +0300

    Значительно обновил error_handlers.py

--- error_handlers.py
+++ error_handlers.py
@@ -1,11 +1,14 @@
-from discord.ext.commands import Cog, NotOwner, NoPrivateMessage, BadArgument
+from discord.ext.commands import Cog, NotOwner, NoPrivateMessage, BadArgument, MissingRequiredArgument
 from discord import Forbidden, NotFound
+from traceback import format_exception
 
 
 class ErrorHandlers(Cog):
     """
     Перехватыватель ошибок, то есть вместо того чтобы отсылать ошибку в консоль,
     Отсылает её пользователю с помощью ctx.send
+
+    P.S. Я не смог придумать как лучше это сделать, если есть идеи напишите мне/сделайте ПР
     """
 
     def __init__(self, bot):
@@ -17,19 +20,26 @@ class ErrorHandlers(Cog):
         if isinstance(exception, NotOwner):
             return await ctx.send(f"Только мой Владелец, {self.bot.app_info.owner}, может использовать эту команду")
 
-        elif isinstance(exception, NoPrivateMessage): return
-        elif isinstance(exception, BadArgument): return
-        elif isinstance(exception, Forbidden): return
-        elif isinstance(exception, NotFound): return
+        elif isinstance(exception, NoPrivateMessage): return await ctx.send('Эта команда доступна только в ЛС')
+        elif isinstance(exception, BadArgument):
+            return await ctx.send('Пожалуйста проверьте правильность написания команды')
+        elif isinstance(exception, MissingRequiredArgument):
+            return await ctx.send('Не хватает необходимого аргумента `%s`' % exception.param.name)
+        elif isinstance(exception, (Forbidden, NotFound)): return
 
         elif "Missing Permissions" in str(exception):
             return await ctx.send("У меня нет необходимых прав для выполнения этой команды. "
                                   "Предоставление мне разрешения администратора должно решить эту проблему")
 
         else:
-            return await ctx.send(
-                f'```\nКоманда: {ctx.command.qualified_name}\n{exception}\n```\n'
-                'Неизвестная ошибка произошла и я не смог выполнить эту команду.')
+            await ctx.send(f'```\nКоманда: {ctx.command.qualified_name}\n{exception}\n```\n'
+                           'Неизвестная ошибка произошла и я не смог выполнить эту команду.\n'
+                           'Я уже сообщил своему создателю')
+            traceback = ''.join(format_exception(type(exception), exception, exception.__traceback__))
+            return await self.bot.app_info.owner.send(
+                'Юзер `%s` нашел ошибку в команде %s.\n'
+                'Traceback: \n```\n%s\n```'
+                % (str(ctx.author), ctx.command.qualified_name, traceback))
 
 
 def setup(bot):
