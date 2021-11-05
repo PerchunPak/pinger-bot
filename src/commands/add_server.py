b5633fa1bbb708c67be799c4e174cdd285586805
PerchunPak <perchunpak@gmail.com>
2021-11-05 15:42:44 +0200

    Убрал неиспользуемые импорты

--- src/commands/add_server.py
+++ src/commands/add_server.py
@@ -1,14 +1,7 @@
-from asyncio import sleep
-from datetime import datetime, timedelta
-from os import mkdir, remove
-from re import sub as re_sub, IGNORECASE
 from socket import gethostbyname, timeout, gaierror
-from sys import version_info
 from asyncpg.exceptions import UniqueViolationError
-from discord import Color, Embed, File
+from discord import Color, Embed
 from discord.ext.commands import Cog, command, is_owner
-from matplotlib.dates import DateFormatter
-from matplotlib.pyplot import subplots, xlabel, ylabel, title
 from mcstatus import MinecraftServer
 
 
--- src/commands/statistic.py
+++ src/commands/statistic.py
@@ -1,12 +1,9 @@
 from asyncio import sleep
 from datetime import datetime, timedelta
 from os import mkdir, remove
-from re import sub as re_sub, IGNORECASE
 from socket import gethostbyname, timeout, gaierror
-from sys import version_info
-from asyncpg.exceptions import UniqueViolationError
 from discord import Color, Embed, File
-from discord.ext.commands import Cog, command, is_owner
+from discord.ext.commands import Cog, command
 from matplotlib.dates import DateFormatter
 from matplotlib.pyplot import subplots, xlabel, ylabel, title
 from mcstatus import MinecraftServer
@@ -105,4 +102,3 @@ class Statistic(Cog):
 
 def setup(bot):
     bot.add_cog(Statistic(bot))
-
