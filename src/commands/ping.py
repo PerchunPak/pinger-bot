9b8fb79bdbeb8407dee00245e3d75a3b04e6aee0
PerchunPak <perchunpak@gmail.com>
2021-11-05 15:03:45 +0200

    Перенес команду "пинг" в файл ping.py

--- src/commands/ping.py
+++ src/commands/ping.py
@@ -17,6 +17,49 @@ class Ping(Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @command(name='пинг')
+    async def ping(self, ctx, ip):
+        """Пинг сервера и показ его основной информации"""
+        print(f'{ctx.author.name} использовал команду "{ctx.message.content}"')
+        embed = Embed(
+            title=f'Пингую {ip}...',
+            description="Подождите немного, я вас упомяну когда закончу",
+            color=Color.orange())
+        await ctx.send(embed=embed)
+        ip_from_alias = await self.bot.db.get_ip_alias(ip)
+        if len(ip_from_alias) != 0:
+            ip = str(ip_from_alias[0]['numip'])[0:-3] + ':' + str(ip_from_alias[0]['port'])
+        server = MinecraftServer.lookup(ip)
+        try:
+            status = server.status()
+            online = True
+        except (timeout, ConnectionRefusedError, gaierror): online, status = False, None
+        if online:
+            num_ip = gethostbyname(server.host)
+            embed = Embed(
+                title=f'Результаты пинга {ip}',
+                description=f"Цифровое айпи: {num_ip}:{str(server.port)}\n**Онлайн**",
+                color=Color.green())
+
+            embed.set_thumbnail(url=f"https://api.mcsrvstat.us/icon/{server.host}:{str(server.port)}")
+            embed.add_field(name="Время ответа", value=str(status.latency) + 'мс')
+            embed.add_field(name="Используемое ПО", value=status.version.name)
+            embed.add_field(name="Онлайн", value=f"{status.players.online}/{status.players.max}")
+            motd_clean = re_sub(r'[\xA7|&][0-9A-FK-OR]', '', status.description, flags=IGNORECASE)
+            embed.add_field(name="Мотд", value=motd_clean)
+            embed.set_footer(text=f'Для получения ссылки на редактирование МОТД, напишите "мотд {ip}"')
+
+            await ctx.send(ctx.author.mention, embed=embed)
+        else:
+            embed = Embed(
+                title=f'Результаты пинга {ip}',
+                description="\n\n**Офлайн**",
+                color=Color.red())
+
+            embed.add_field(name="Не удалось пингануть сервер",
+                            value='Возможно вы указали неверный айпи, или сервер сейчас выключен')
+            await ctx.send(ctx.author.mention, embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Ping(bot))
