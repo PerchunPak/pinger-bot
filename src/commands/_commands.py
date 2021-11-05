7e7af22441f55d6bc9054f208e92abe114cc1f49
PerchunPak <perchunpak@gmail.com>
2021-11-05 14:49:18 +0200

    Добавил файлы команд и им паттерн

---
+++ src/commands/_commands.py
@@ -0,0 +1,10 @@
+class GlobalFuncsCommands:
+    """
+    Общие функции (или как их там в ООП) для комманд
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def parse_ip(self, ip):
+        pass
---
+++ src/commands/alias.py
@@ -0,0 +1,22 @@
+from asyncio import sleep
+from datetime import datetime, timedelta
+from os import mkdir, remove
+from re import sub as re_sub, IGNORECASE
+from socket import gethostbyname, timeout, gaierror
+from sys import version_info
+from asyncpg.exceptions import UniqueViolationError
+from discord import Color, Embed, File
+from discord.ext.commands import Cog, command, is_owner
+from matplotlib.dates import DateFormatter
+from matplotlib.pyplot import subplots, xlabel, ylabel,>
+from mcstatus import MinecraftServer
+
+
+class Alias(Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(Alias(bot))
