32c0ac610bd01dd89d35c3a8842aaa81d54ace41
PerchunPak <perchunpak@gmail.com>
2021-11-06 13:13:08 +0200

    Вернул тесты
    This reverts commit 829a2221ce211751a0b81ee39e7b39349cf72f30.

---
+++ tests/test_cmd_alias.py
@@ -0,0 +1,76 @@
+"""
+Тесты для команды "алиас"
+"""
+from time import sleep
+from discord.ext.test import message, get_embed
+from pytest import fixture, mark
+from discord import Color
+
+
+class TestAlias:
+    """Класс для тестов и фикстур"""
+    @fixture(scope='class')
+    async def alias_added_not_owner(self, event_loop, bot, database, monkeypatch_session):
+        """Фикстура для тестов если сервер добавлен, но юзер не владелец"""
+        await database.add_server('127.0.0.7', 0, 25565)
+        await message("алиас тест1 127.0.0.7")
+        embed = get_embed()
+        while str(embed.color) == str(Color.orange()):  # ждет пока бот не отошлет результаты вместо
+            sleep(0.01)                                 # "ожидайте, в процессе"
+            embed = get_embed()
+
+        return embed
+
+    @fixture(scope='class')
+    async def alias_added(self, event_loop, bot, database, monkeypatch_session):
+        """Фикстура для тестов если алиас добавлен"""
+        test_user = None
+        for user in bot.users:
+            if user.bot: continue
+            test_user = user
+        await database.add_server('127.0.0.8', test_user.id, 25565)
+        await message("алиас тест2 127.0.0.8")
+        embed = get_embed()
+        while str(embed.color) == str(Color.orange()):  # ждет пока бот не отошлет результаты вместо
+            sleep(0.01)                                 # "ожидайте, в процессе"
+            embed = get_embed()
+
+        return embed
+
+    @fixture(scope='class')
+    async def alias_not_added(self, event_loop, bot, database, monkeypatch_session):
+        """Фикстура для тестов если сервер не добавлен"""
+        await message("алиас тест3 127.0.0.9")
+        embed = get_embed()
+        while str(embed.color) == str(Color.orange()):  # ждет пока бот не отошлет результаты вместо
+            sleep(0.01)                                 # "ожидайте, в процессе"
+            embed = get_embed()
+
+        return embed
+
+    @mark.asyncio
+    async def test_is_added_to_db(self, event_loop, bot, database, alias_added):
+        """Проверяет добавился ли алиас в дата базу"""
+        ip_from_alias = await database.get_ip_alias('тест2')
+        ip_from_alias = str(ip_from_alias[0]['numip'])[0:-3] + ':' + str(ip_from_alias[0]['port'])
+        assert ip_from_alias == '127.0.0.8:25565'
+
+    def test_color(self, bot, database, alias_added):
+        """Проверят цвет в ответе бота"""
+        assert str(alias_added.color) == str(Color.green())
+
+    def test_not_owner(self, bot, database, alias_added_not_owner):
+        """Проверяет правильно ли бот понимает кто владелец"""
+        assert alias_added_not_owner.fields[0].value == 'Вы не владелец'
+
+    def test_not_owner_color(self, bot, database, alias_added_not_owner):
+        """Проверяет цвет Embed-а если алиас добавлен, и юзер не владелец"""
+        assert str(alias_added_not_owner.color) == str(Color.red())
+
+    def test_not_added(self, bot, database, alias_not_added):
+        """Проверяет правильно ли бот распознает если сервере еще не добавлен"""
+        assert alias_not_added.fields[0].name == 'Не удалось добавить алиас'
+
+    def test_not_added_color(self, bot, database, alias_not_added):
+        """Проверяет цвет Embed-а когда сервер не добавлен"""
+        assert str(alias_not_added.color) == str(Color.red())
---
+++ tests/test_other_cmd.py
@@ -0,0 +1,73 @@
+"""
+Тесты для других команд
+"""
+from discord.ext.test import message, get_message, get_embed
+from pytest import fixture
+from sys import version_info
+
+
+class TestOtherCommands:
+    """Класс для тестов и фикстур"""
+
+    @fixture(scope='class')
+    async def help_cmd(self, event_loop, bot):
+        """Фикстура для проверки команды "помощь"."""
+        await message("помощь")
+        return get_embed().to_dict()
+
+    @fixture(scope='class')
+    async def about(self, event_loop, bot):
+        """Фикстура для проверки команды "инфо"."""
+        await message("инфо")
+        return get_embed()
+
+    @fixture(scope='class')
+    async def invite(self, event_loop, bot):
+        """Фикстура для проверки команды "пригласить"."""
+        await message("пригласить")
+        return get_message()
+
+    def test_help_commands(self, bot, help_cmd):
+        """Проверяет правильно ли бот распознает команды."""
+        i = 0
+        commands = sorted([c for c in bot.commands if not c.hidden], key=lambda c: c.name)
+        for field in help_cmd['fields']:
+            assert {'inline': False, 'name': commands[i].name, 'value': commands[i].help} == field
+            i += 1
+
+    def test_about_description(self, bot, about):
+        """Проверят правильно ли бот распознает описание в дискорде."""
+        assert bot.app_info.description in about.description
+
+    def test_about_id(self, bot, about):
+        """Проверят правильно ли бот распознает свой айди."""
+        assert str(bot.app_info.id) in about.description
+
+    def test_about_owner(self, bot, about):
+        """Проверят правильно ли бот распознает владельца."""
+        assert str(bot.app_info.owner) == about.fields[0].value
+
+    def test_about_count_servers_int(self, bot, about):
+        """
+        Проверят int ли количество серверов.
+        Если не int, оно выдаст ошибку при переводе в int.
+        А если просто написать int(something()), будет не понятно из за чего тест проваливается.
+        """
+        assert isinstance(int(about.fields[1].value), int)
+
+    def test_about_count_users_int(self, bot, about):
+        """
+        Проверят int ли количество пользователей.
+        Если не int, оно выдаст ошибку при переводе в int.
+        А если просто написать int(something()), будет не понятно из за чего тест проваливается.
+        """
+        assert isinstance(int(about.fields[2].value), int)
+
+    def test_about_python_version(self, bot, about):
+        """Проверят правильно ли бот распознает версию Python."""
+        assert '.'.join(map(str, version_info[:3])) in about.fields[3].value
+
+    def test_invite_link(self, bot, invite):
+        """Проверят правильно ли бот создает ссылку-приглашение."""
+        assert f"https://discordapp.com/oauth2/authorize?client_id={bot.app_info.id}&scope=bot&permissions=8" \
+               in invite.content
