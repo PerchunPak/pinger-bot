2ff5dcf7a4cf88148c058f9d2f6586cd75bddb5a
PerchunPak <perchunpak@gmail.com>
2021-11-06 14:07:27 +0200

    Переписал команду "мотд"

--- src/commands/motd.py
+++ src/commands/motd.py
@@ -1,40 +1,28 @@
-from socket import timeout, gaierror
 from discord import Color, Embed
 from discord.ext.commands import Cog, command
-from mcstatus import MinecraftServer
+from src.commands._commands import MetodsForCommands
 
 
 class Motd(Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.MetodsForCommands = MetodsForCommands(bot)
 
     @command(name='мотд')
     async def motd(self, ctx, ip):
         """Показывает мотд и ссылку на редактирование сервера"""
-        print(f'{ctx.author.name} использовал команду "{ctx.message.content}"')
-        embed = Embed(
-            title=f'Получаю данные сервера {ip}...',
-            description="Подождите немного, я вас упомяну когда закончу",
-            color=Color.orange())
-        await ctx.send(embed=embed)
-        ip_from_alias = await self.bot.db.get_ip_alias(ip)
-        if len(ip_from_alias) != 0:
-            ip = str(ip_from_alias[0]['numip'])[0:-3] + ':' + str(ip_from_alias[0]['port'])
-        server = MinecraftServer.lookup(ip)
-        try:
-            status = server.status()
-            online = True
-        except (timeout, ConnectionRefusedError, gaierror): online, status = False, None
-        if online:
-            motd = str(status.raw['description']['text']).replace(' ', '+')
-            motd_url = motd.replace('\n', '%0A')
+        await self.MetodsForCommands.wait_please(ctx, ip)
+        status, dns_info, info = await self.MetodsForCommands.ping_server(ip)
+        if status:
+            motd = str(status.raw["description"]["text"]).replace(" ", "+")
+            motd_url = motd.replace("\n", "%0A")
             embed = Embed(
                 title=f'Подробное мотд сервера {ip}',
                 description="Эта команда дает возможность скопировать мотд и вставить на свой сервер",
                 color=Color.green())
 
-            embed.set_thumbnail(url=f"https://api.mcsrvstat.us/icon/{server.host}:{str(server.port)}")
+            embed.set_thumbnail(url=f"https://api.mcsrvstat.us/icon/{ip}")
             embed.add_field(name="Мотд", value=f"{status.description}")
             embed.add_field(name="Ссылка на редактирование", value="https://mctools.org/motd-creator?text=" + motd_url)
             await ctx.send(ctx.author.mention, embed=embed)
