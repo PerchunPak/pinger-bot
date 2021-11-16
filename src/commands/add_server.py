3b0b47e1b7f68a5027df0a770796b89ca9c101c2
PerchunPak <perchunpak@gmail.com>
2021-11-16 19:17:34 +0200

    Алиаса не может быть при использовании "добавить"

--- src/commands/add_server.py
+++ src/commands/add_server.py
@@ -32,11 +32,10 @@ class AddServer(Cog):
         await self.metods_for_commands.wait_please(ctx, ip)
         status, dns_info, info = await self.metods_for_commands.ping_server(ip)
         if status:
-            name = info.alias if info.alias is not None else ip
             try: await self.bot.db.add_server(info.ip, dns_info.port, ctx.author.id)
             except UniqueViolationError:  # сервер уже добавлен
                 embed = Embed(
-                    title=f'Не удалось добавить сервер {name}',
+                    title=f'Не удалось добавить сервер {ip}',
                     description="**Онлайн**",
                     color=Color.red())
 
@@ -45,14 +44,14 @@ class AddServer(Cog):
                 return await ctx.send(ctx.author.mention, embed=embed)
 
             embed = Embed(
-                title=f'Добавил сервер {name}',
+                title=f'Добавил сервер {ip}',
                 description=f"Цифровое айпи: {info.ip}:{str(dns_info.port)}\n**Онлайн**",
                 color=Color.green())
 
             embed.set_thumbnail(url=f"https://api.mcsrvstat.us/icon/{info.ip}:{str(dns_info.port)}")
             embed.add_field(name="Сервер успешно добавлен",
                             value='Напишите "помощь" для получения большей информации о серверах')
-            embed.set_footer(text=f'Теперь вы можете использовать "стата {name}" или "алиас (алиас) {ip}"')
+            embed.set_footer(text=f'Теперь вы можете использовать "стата {ip}" или "алиас (алиас) {ip}"')
 
             await ctx.send(ctx.author.mention, embed=embed)
         else:
