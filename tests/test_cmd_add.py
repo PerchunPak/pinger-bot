32e894b3f154b7d21e6e009acbd261f23e672809
PerchunPak <perchunpak@gmail.com>
2021-12-09 12:43:42 +0200

    Добавил тесты для add если сервер уже

--- tests/test_cmd_add.py
+++ tests/test_cmd_add.py
@@ -102,6 +102,60 @@ class TestAddServer:
 
         monkeypatch_session.setattr(MinecraftServer, "status", fake_server_answer)
         monkeypatch_session.setattr(bot, "is_owner", fake_is_owner)
+        await message("добавить example.com")
+        embed = get_embed()
+        while str(embed.color) == str(Color.orange()):  # ждет пока бот не отошлет результаты вместо
+            sleep(0.01)                                 # "ожидайте, в процессе"
+            embed = get_embed()
+
+        return embed
+
+    @staticmethod
+    @fixture(scope='class')
+    async def add_already(event_loop, bot, database, monkeypatch_session):
+        """Пытается добавить уже добавленный сервер.
+
+        Args:
+            event_loop: Обязательная фикстура для async фикстур.
+            bot: Главный объект бота.
+            database: Объект дата базы.
+            monkeypatch_session: `monkeypatch` фикстура только с scope='session'.
+
+        Returns:
+            Embed объект ответа.
+        """
+        def fake_server_answer(class_self=None) -> PingResponse:
+            """Эмулирует ответ сервера.
+
+            Args:
+                class_self: Иногда при вызове функции, так же приходит аргумент `self`.
+
+            Returns:
+                Фейковый ответ сервера.
+            """
+            return PingResponse(
+                {
+                    "description": {'text': "A Minecraft Server"},
+                    "players": {"max": 20, "online": 5},
+                    "version": {"name": "1.17.1", "protocol": 756},
+                }
+            )
+
+        async def fake_is_owner(class_self=None, author_id=None) -> bool:
+            """Эмулирует ответ функции проверки автора.
+
+            Args:
+                class_self: Иногда при вызове функции, так же приходит аргумент `self`.
+                author_id: Айди пользователя который приходит оригинальной функции.
+
+            Returns:
+                True.
+            """
+            return True
+
+        monkeypatch_session.setattr(MinecraftServer, "status", fake_server_answer)
+        monkeypatch_session.setattr(bot, "is_owner", fake_is_owner)
+        await database.add_server("127.0.0.34", 25565, 0)
         await message("добавить 127.0.0.34")
         embed = get_embed()
         while str(embed.color) == str(Color.orange()):  # ждет пока бот не отошлет результаты вместо
@@ -154,3 +208,20 @@ class TestAddServer:
             add_offline: Embed объект ответа.
         """
         assert str(add_offline.color) == str(Color.red())
+
+    @staticmethod
+    def test_already_color(add_already):
+        """Проверят цвет в ответе бота, если сервер уже добавлен.
+
+        Args:
+            add_already: Embed объект ответа.
+        """
+        assert str(add_already.color) == str(Color.red())
+
+    @staticmethod
+    def test_already_ip_in_title(add_already):
+        """Проверяет что есть айпи в title.
+        Args:
+            add_already: Embed объект ответа.
+        """
+        assert "127.0.0.34" in add_already.title
