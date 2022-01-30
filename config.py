696d65f4d6c272c1cf037bcf501bf1a2f223e279
PerchunPak <68118654+PerchunPak@users.noreply.github.com>
2022-01-30 11:09:19 +0200

    sqlalchemy понимает только postgresql Без ql в конце, больше нельзя

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import environ
 TOKEN = environ.get("PINGERBOT_DISCORD_TOKEN", "-->TOKEN_HERE<--")
 """Дискорд токен бота."""
 
-POSTGRES = environ.get("PINGERBOT_POSTGRES", "postgres://[user]:[password]@[host]:[port]/[database name]")
+POSTGRES = environ.get("PINGERBOT_POSTGRES", "postgresql://[user]:[password]@[host]:[port]/[database name]")
 """
 Данные от Postgres (СУБД). Это необходимо, чтобы бот мог хранить данные в базе данных SQL.
 Данные выглядят так: "postgres://[user]:[password]@[host]:[port]/[database name]" (Некоторые данные не обязательные).
