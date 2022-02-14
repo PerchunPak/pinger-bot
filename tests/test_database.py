7259ea7a6efbd624ad0cadd8ebe731de56e566b0
PerchunPak <68118654+PerchunPak@users.noreply.github.com>
2022-02-14 13:31:20 +0200

    Добавил тест для db.get_ip_alias, если сервер не добавлен

--- tests/test_database.py
+++ tests/test_database.py
@@ -151,6 +151,16 @@ class TestGetFunctions:
         right_answer = database.execute(select(database.t.ss).where(database.t.ss.c.alias == "тест123")).one()
         assert answer == right_answer
 
+    async def test_get_ip_alias_null(self, database):
+        """Проверяет метод get_ip_alias, если сервера нету.
+
+        Args:
+            database: Объект дата базы.
+        """
+        database.make_tables()
+        answer = database.get_ip_alias("тест1231")
+        assert answer == {}
+
     async def test_get_pings(self, database):
         """Проверяет метод get_pings.
 
