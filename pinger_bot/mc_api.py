ce00ee6985af50965b0a894df3874af90301b5b4
PerchunPak <perchunpak@gmail.com>
2022-05-18 21:26:12 +0200

    Make from `MCServer` dataclass

--- pinger_bot/mc_api.py
+++ pinger_bot/mc_api.py
@@ -21,39 +21,39 @@ class StatusError(Exception):
     """Raised by ``MCServer.status`` when something went wrong."""
 
 
+@dataclass
 class MCServer:
     """Represents an MineCraft Server, doesn't connected to platform."""
 
-    def __init__(
-        self,
-        host: str,
-        port: int,
-        motd: str,
-        version: str,
-        players: Players,
-        latency: float,
-    ) -> None:
-        """__init__ method.
+    #: Host where server is, like ``127.0.0.1``.
+    host: str
+    #: Port of the server, example ``25565`` or ``19132``.
+    port: int
+    #: MOTD of the server.
+    motd: str
+    #: Name of the version, example ``1.18`` or ``1.7``.
+    version: str
+    #: ``Players`` object.
+    players: Players
+    #: Time of response from server, in MS.
+    latency: float
 
-        Args:
-            host: Host where server is, like ``127.0.0.1``.
-            port: Port of the server, example ``25565`` or ``19132``.
-            motd: MOTD of the server.
-            version: Name of the version, example ``1.18`` or ``1.7``.
-            players: ``Players`` object.
-            latency: Time of response from server, in MS.
+    #: Icon of the server.
+    icon: str = None  # type: ignore[assignment] # will be set in __post_init__
+    #: Number IP of the server.
+    num_ip: str = None  # type: ignore[assignment] # will be set in __post_init__
+
+    def __post_init__(self) -> None:
+        """Post init method.
 
         Examples:
             See examples in ``MCServer.handle_java`` or ``handle_bedrock`` methods.
         """
-        self.host = host
-        self.port = port
-        self.num_ip = self.host + ":" + str(self.port)
-        self.motd = motd
-        self.version = version
-        self.players = players
-        self.image = f"https://api.mcsrvstat.us/icon/{self.host}:{str(self.port)}"
-        self.latency = latency
+        # it is reachable if user not defined this fields
+        if self.icon is None:
+            self.icon = f"https://api.mcsrvstat.us/icon/{self.host}:{str(self.port)}"  # type: ignore[unreachable]
+        if self.num_ip is None:
+            self.num_ip = self.host + ":" + str(self.port)  # type: ignore[unreachable]
 
     @classmethod
     async def status(cls, host: str) -> "MCServer":
